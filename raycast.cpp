ce3d8685ad462751be4a2f498b214201b142fc5f
Carson Shah <cas2343@nau.edu>
2026-03-19 09:43:10 -0700

    started raytracing

--- raycast.cpp
+++ raycast.cpp
@@ -7,6 +7,7 @@
 #include <stdint.h>
 #define PLANEDISTANCEZ -1
 #define PI 3.141592
+#define MAXLEVEL 7 
 using namespace std;
 
 //classes:
@@ -95,6 +96,7 @@ class object{
         float c_diff[3]={0,0,0};
         float c_spec[3]={0,0,0};
         float position[3]={0,0,0}; 
+        float reflection =0.0; 
         
 
 
@@ -191,6 +193,9 @@ class sphere: public object{
                 else if(strcmp(buffer, "radius:")==0){
                     fscanf(scene, " %f ", &radius);
                 }
+                else if(strcmp(buffer, "reflection:")==0){
+                    fscanf(scene, " %f ", &reflection);
+                }
             }while(strcmp(buffer,";")!=0);
 
         }
@@ -252,6 +257,9 @@ class plane: public object{
                 }
                 else if(strcmp(buffer, "normal:")==0){
                     fscanf(scene, " %f %f %f ", &normal[0], &normal[1], &normal[2]);
+                }
+                else if(strcmp(buffer, "reflection:")==0){
+                    fscanf(scene, " %f ", &reflection);
                 }
             }while(strcmp(buffer,";")!=0);
 
@@ -349,6 +357,201 @@ void find_current_Ray(int current_w, int current_h, float pix_width, float pix_h
     v3_normalize(current_ray->direction, current_ray->direction);
 }
 
+//function: CalculateLight 
+//input: target_object
+//output: float* I(Illumination)
+void Calculate_Light(object* target_object, ray* current_ray, float lowest_t,
+    list<object*> *scene_information, list<light*> *light_information, float* I){
+
+    //set points and variables for lighting 
+    float distance =0; 
+    float intersection_normal[3]={0,0,0};
+    float intersection_point[3]={0,0,0};
+    current_ray->find_intersection_point(lowest_t, intersection_point);
+    target_object->get_normal(intersection_normal,intersection_point);
+    float shadow_origin[3] = {
+    intersection_point[0] + intersection_normal[0] * 0.0001f,
+    intersection_point[1] + intersection_normal[1] * 0.0001f,
+    intersection_point[2] + intersection_normal[2] * 0.0001f
+    };
+    // apply effects for each light in scene
+    for(light* current_light : *light_information){
+
+        // construct ray from intersection to light
+        float L_vector[3];
+        v3_from_points(L_vector, intersection_point, current_light->position);
+        distance = v3_length(L_vector);
+        v3_normalize(L_vector, L_vector);
+
+
+        bool is_shadowed = false;
+
+
+        //find objects blocking the light 
+        for(object* current_object: *scene_information){
+            if(current_object==target_object){
+                continue; 
+            }
+            float t = current_object->find_intersection(shadow_origin, L_vector);
+            // if the object is closer, current object is shadowed
+            if(t < distance&& t>0.00001f){
+                //set as shadowed
+                is_shadowed = true;
+                //DEBUG
+                //printf("DEBUG: is shadowed\n");
+                break; 
+            }
+        }
+        
+        if(is_shadowed){
+            // this pixel is not effect by this light
+            continue;
+        }
+        else{
+            //Light the scene
+            //printf("DEBUG: LIGHTING Started");
+            // radial attenuation
+            float f_rad = 1.0f;
+
+            float denom = current_light->radial_a0 +
+                        current_light->radial_a1 * distance +
+                        current_light->radial_a2 * distance * distance;
+
+            if(denom > 0.0f)
+                f_rad = 1.0f / denom;
+
+            // angular attenuation
+            float f_ang = 1.0f;
+
+            if(current_light->theta > 0.0f)
+            {
+                float VL[3];
+                float VO[3];
+
+                // VL = direction from light to surface
+                v3_from_points(VL, current_light->position, intersection_point);
+                v3_normalize(VL, VL);
+
+                // VO = spotlight direction (should already point outward)
+                VO[0] = current_light->direction[0];
+                VO[1] = current_light->direction[1];
+                VO[2] = current_light->direction[2];
+                v3_normalize(VO, VO);
+
+                float cos_alpha = v3_dot_product(VO, VL);
+                float cos_theta = cos(current_light->theta * PI / 180.0f);
+
+                if(cos_alpha < cos_theta)
+                    f_ang = 0.0f;
+                else
+                    f_ang = pow(cos_alpha, current_light->angular_a0);
+            }
+
+            //calculate light values 
+            v3_normalize(L_vector, L_vector);
+            float normal[3]={0,0,0};
+            target_object->get_normal(normal,intersection_point);
+
+            float view_vector[3]=
+                {-current_ray->direction[0],
+                -current_ray->direction[1],
+                -current_ray->direction[2]};
+
+            float L_in[3] = {
+                -L_vector[0],
+                -L_vector[1],
+                -L_vector[2]
+            };
+
+            float reflection[3] = {0,0,0};
+            v3_reflect(reflection, L_in, normal);
+            
+            float normal_dot_ray = v3_dot_product(normal, L_vector);
+            float view_dot_reflection = v3_dot_product(view_vector, reflection);
+            
+            float I_diff[3]={0,0,0};
+            float I_spec[3]={0,0,0};
+
+            if(normal_dot_ray > 0){
+                // calculate diffusion for RGB
+                for(int c = 0; c < 3; c++){
+                    I_diff[c] += target_object->c_diff[c] *
+                            current_light->color[c] *
+                            normal_dot_ray;
+                }
+            }
+            if(view_dot_reflection > 0 && normal_dot_ray > 0)
+            {
+                // calculate specular for RGB
+                float spec = pow(view_dot_reflection, current_light->ns);
+
+                for(int c = 0; c < 3; c++)
+                {
+                    I_spec[c] += target_object->c_spec[c] *
+                            current_light->color[c] *
+                            spec;
+                }
+            }
+
+            I[0] += f_rad * f_ang * (I_spec[0] + I_diff[0]);
+            I[1] += f_rad * f_ang * (I_spec[1] + I_diff[1]);
+            I[2] += f_rad * f_ang * (I_spec[2] + I_diff[2]);
+            for(int c = 0; c < 3; c++)
+            {
+                if(I[c] > 1.0f) I[c] = 1.0f;
+                if(I[c] < 0.0f) I[c] = 0.0f;
+            }
+        }
+    }
+
+}
+
+//function: rayCast(recursive)
+//input:
+//output:
+void Shade( ray *current_ray,list<object*> *scene_information, 
+    list<light*> *light_information, float* Color, int level){
+    //base case: hits max cases 
+    if(level>=MAXLEVEL){
+
+        return; 
+    }
+    else{
+        object *target_object = NULL;
+        float lowest_t = INFINITY;
+        //find closest object 
+        for(object* current_object : *scene_information){
+            //DEBUG: check position
+            //printf("current object position: %f %f %f \n", current_object->position[0], current_object->position[1], current_object->position[2]);
+            float current_t = current_object->find_intersection(current_ray->origin, current_ray->direction);
+            if(current_t > 0 && current_t < lowest_t)
+            {
+                target_object = current_object;
+                lowest_t=current_t;
+            }
+        }
+        if(target_object!=NULL){
+            //func: calculateLight 
+            //find reflection ray
+            ray reflection;
+            float normal[3]={0,0,0}; 
+            target_object->get_normal(normal, current_ray->origin);
+            v3_reflect(reflection.direction, current_ray->direction, normal);
+            current_ray->find_intersection_point(lowest_t ,reflection.origin);
+
+            //find current ray's color
+            Shade(&reflection, scene_information,light_information,Color,level+1);
+            Calculate_Light(target_object, current_ray, lowest_t, scene_information, light_information, Color);
+            Color[0]*=target_object->reflection;
+            Color[1]*=target_object->reflection;
+            Color[2]*=target_object->reflection;
+        }
+        Calculate_Light(target_object, current_ray, lowest_t, scene_information, light_information, Color);
+        return;
+        //chat, recurse this guy
+    }
+}
+
 
 //function: ppm_write 
 void ppm_write(char *filename, uint8_t **pixmap, int height, int width, int max_colors)
@@ -372,6 +575,8 @@ void ppm_write(char *filename, uint8_t **pixmap, int height, int width, int max_
     fclose(out);
 }
 
+//function: find col 
+
 //main:
 int main (int argc, char* argv[]){
     if (argc!=5){
@@ -391,200 +596,30 @@ int main (int argc, char* argv[]){
     list<object*> *scene_information = new list<object*>;
     list<light*> *light_information = new list<light*>;
 
+
     //func: read scene
+
     read_scene(scene_information,light_information, argv[3],current_camera);
     printf("file read! \n");
     assert(current_camera!=NULL);
     float pix_height= current_camera->height/image_info.height; 
     float pix_width= current_camera->width/image_info.width; 
     ray *current_ray = new ray; 
-
     //loop through pixels
     for(int h=0; h<image_info.height; h++){
         for(int w=0; w<image_info.width; w++){
-            
-            object *target_object = NULL;
-            float intersection_point[3]={0,0,0};
-            float lowest_t = INFINITY;
-            //func: findRay 
-            find_current_Ray(w, h,pix_width, pix_height, *current_camera, image_info, current_ray);
-            
-            //find closest object 
-            for(object* current_object : *scene_information){
-        
-                //DEBUG: check position
-                //printf("current object position: %f %f %f \n", current_object->position[0], current_object->position[1], current_object->position[2]);
-                float current_t = current_object->find_intersection(current_ray->origin, current_ray->direction);
-                if(current_t > 0 && current_t < lowest_t)
-                {
-                    target_object = current_object;
-                    lowest_t=current_t;
-                    //DEBUG: chheck intersection
-                    //printf("intersection found at (%d %d)\n", h, w);
-                }
+            find_current_Ray(w,h,pix_width,pix_height,*current_camera,image_info,current_ray);
+            float I[3]= {0,0,0};
+            //func: Shade (raytrace)
+            Shade(current_ray,scene_information,light_information,I,1);
 
+            //find index
+            int pixel_index=(h*image_info.width+w)*3; 
+            for(int c = 0; c < 3; c++)
+            {
+                pixmap[pixel_index + c] = (uint8_t)(I[c] * 255);
             }
-            if(target_object!=NULL){
-            //pull object info
-                float I[3]={0,0,0};
-                float distance =0; 
-                float intersection_normal[3]={0,0,0};
-                target_object->get_normal(intersection_normal,intersection_point);
-                float shadow_origin[3] = {
-                intersection_point[0] + intersection_normal[0] * 0.0001f,
-                intersection_point[1] + intersection_normal[1] * 0.0001f,
-                intersection_point[2] + intersection_normal[2] * 0.0001f
-                };
-                current_ray->find_intersection_point(lowest_t, intersection_point);
-
-                // apply effects for each light in scene
-                for(light* current_light : *light_information){
-
-                    // construct ray from intersection to light
-                    float L_vector[3];
-                    v3_from_points(L_vector, intersection_point, current_light->position);
-                    distance = v3_length(L_vector);
-                    v3_normalize(L_vector, L_vector);
-
-
-                    bool is_shadowed = false;
-
-        
-                    //find objects blocking the light 
-                    for(object* current_object: *scene_information){
-                        if(current_object==target_object){
-                            continue; 
-                        }
-                        float t = current_object->find_intersection(shadow_origin, L_vector);
-                        // if the object is closer, current object is shadowed
-                        if(t < distance&& t>0.00001f){
-                            //set as shadowed
-                            is_shadowed = true;
-                            //DEBUG
-                            //printf("DEBUG: is shadowed\n");
-                            break; 
-                        }
-                    }
-                    
-                    if(is_shadowed){
-                        // this pixel is not effect by this light
-                        continue;
-                    }
-                    else{
-                        //Light the scene
-                        //printf("DEBUG: LIGHTING Started");
-                        // radial attenuation
-                        float f_rad = 1.0f;
-
-                        float denom = current_light->radial_a0 +
-                                    current_light->radial_a1 * distance +
-                                    current_light->radial_a2 * distance * distance;
-
-                        if(denom > 0.0f)
-                            f_rad = 1.0f / denom;
-
-                        // angular attenuation
-                        float f_ang = 1.0f;
-
-                        if(current_light->theta > 0.0f)
-                        {
-                            float VL[3];
-                            float VO[3];
-
-                            // VL = direction from light to surface
-                            v3_from_points(VL, current_light->position, intersection_point);
-                            v3_normalize(VL, VL);
-
-                            // VO = spotlight direction (should already point outward)
-                            VO[0] = current_light->direction[0];
-                            VO[1] = current_light->direction[1];
-                            VO[2] = current_light->direction[2];
-                            v3_normalize(VO, VO);
-
-                            float cos_alpha = v3_dot_product(VO, VL);
-                            float cos_theta = cos(current_light->theta * PI / 180.0f);
-
-                            if(cos_alpha < cos_theta)
-                                f_ang = 0.0f;
-                            else
-                                f_ang = pow(cos_alpha, current_light->angular_a0);
-                        }
-
-                        //calculate light values 
-                        v3_normalize(L_vector, L_vector);
-                        float normal[3]={0,0,0};
-                        target_object->get_normal(normal,intersection_point);
-
-                        float view_vector[3]=
-                            {-current_ray->direction[0],
-                            -current_ray->direction[1],
-                            -current_ray->direction[2]};
-
-                        float L_in[3] = {
-                            -L_vector[0],
-                            -L_vector[1],
-                            -L_vector[2]
-                        };
-
-                        float reflection[3] = {0,0,0};
-                        v3_reflect(reflection, L_in, normal);
-                        
-                        float normal_dot_ray = v3_dot_product(normal, L_vector);
-                        float view_dot_reflection = v3_dot_product(view_vector, reflection);
-                        
-                        float I_diff[3]={0,0,0};
-                        float I_spec[3]={0,0,0};
-
-                        if(normal_dot_ray > 0){
-                            // calculate diffusion for RGB
-                            for(int c = 0; c < 3; c++){
-                                I_diff[c] += target_object->c_diff[c] *
-                                        current_light->color[c] *
-                                        normal_dot_ray;
-                            }
-                        }
-                        if(view_dot_reflection > 0 && normal_dot_ray > 0)
-                        {
-                            // calculate specular for RGB
-                            float spec = pow(view_dot_reflection, current_light->ns);
-
-                            for(int c = 0; c < 3; c++)
-                            {
-                                I_spec[c] += target_object->c_spec[c] *
-                                        current_light->color[c] *
-                                        spec;
-                            }
-                        }
-
-                        I[0] += f_rad * f_ang * (I_spec[0] + I_diff[0]);
-                        I[1] += f_rad * f_ang * (I_spec[1] + I_diff[1]);
-                        I[2] += f_rad * f_ang * (I_spec[2] + I_diff[2]);
-                        for(int c = 0; c < 3; c++)
-                        {
-                            if(I[c] > 1.0f) I[c] = 1.0f;
-                            if(I[c] < 0.0f) I[c] = 0.0f;
-                        }
-                    }
-                }
-
-                //set color in pixmap 
-                int pixel_index=(h*image_info.width+w)*3; 
-                
-                //determine color 
-                if(target_object==NULL){
-                    pixmap[pixel_index]=0;
-                    pixmap[pixel_index+1]=0;
-                    pixmap[pixel_index+2]=0;
-                }
-                else{
-                    for(int c = 0; c < 3; c++)
-                    {
-                        pixmap[pixel_index + c] = (uint8_t)(I[c] * 255);
-                    }
-                }
-            }
- 
-          
+            
         }
     }
 
